5471a84d0141c21893d41d8689e07e02058cc135
Ric254 <eerickmwangi@gmail.com>
2022-11-17 00:06:51 -0800

    simple shell

--- shell.h
+++ shell.h
@@ -4,6 +4,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
